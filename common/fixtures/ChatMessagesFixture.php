8a2f32c8ea1bacf3871c8553d207e3e728a057f2
Evgeniy <e.malashenko@whileweb.com>
2020-07-22 16:32:04 +0300

    control commit

---
+++ common/fixtures/ChatMessagesFixture.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace common\fixtures;
+
+
+use yii\test\ActiveFixture;
+
+class ChatMessagesFixture extends ActiveFixture
+{
+  public $modelClass = 'frontend\models\ChatMessages';
+}
